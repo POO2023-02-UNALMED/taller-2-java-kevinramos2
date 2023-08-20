a6e7e8330f0d3dc05de3a18cb87e4fc8bf6b4a0d
kevinramos2 <keramosl@unal.edu.co>
2023-08-20 14:41:08 -0500

    asiento2

--- src/test/java/test/asiento.java
+++ src/test/java/test/asiento.java
@@ -6,7 +6,7 @@ public class asiento {
 	int registro;
 	
 	void cambiarColor(String color) {
-		if (color =="rojo"||color =="verde"||color =="amarillo"||color =="negro"||color =='blanco') {
+		if (color =="rojo"||color =="verde"||color =="amarillo"||color =="negro"||color =="blanco") {
 			this.color=color;
 		}
 	}
