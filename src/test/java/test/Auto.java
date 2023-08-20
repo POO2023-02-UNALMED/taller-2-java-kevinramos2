4af7cfec996c98a5533e4bf874835183b8296cae
kevinramos2 <keramosl@unal.edu.co>
2023-08-20 15:01:09 -0500

    auto12

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,5 +1,36 @@
 package test;
 
 public class Auto {
-
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	int cantidadAsientos() {
+		int numeroAsientos = 0;
+		
+		for (int i = 0 ; i < asientos.length;i++) {
+			if (asientos[i] != null) {
+				numeroAsientos++;
+			}
+		}
+		return numeroAsientos;
+	}
+	String verificarIntegridad() {
+		if (registro == motor.registro) {
+			for (int i = 0; i<asientos.length; i++) {
+				if (asientos[i])!= null){
+					if(asientos[i].registro != registro) {
+						return "Las piezas no son originales";
+					}
+				}
+			}
+			return "Auto original";	
+			}
+		else {
+			return "las piezas no son originales";
+		}
+	}
 }
